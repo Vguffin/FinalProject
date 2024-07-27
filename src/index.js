224785672ceb0ab9260411801eb304c70a99896b
Vguffin <135627490+Vguffin@users.noreply.github.com>
2024-07-26 23:58:32 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { marked } from 'marked';
+import './styles.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+document.addEventListener('DOMContentLoaded', () => {
+  const editor = document.getElementById('editor');
+  const preview = document.getElementById('preview');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  function updatePreview() {
+      const markdownText = editor.value;
+      const htmlContent = marked(markdownText);
+      preview.innerHTML = htmlContent;
+  }
+
+//  editor
+  editor.addEventListener('input', updatePreview);
+
+  updatePreview();
+});
